f946fcf1c1bbe9d8c5220eb6a9616e72228ce932
TheDarkWolf <franciscopugh01@gmail.com>
2019-08-04 02:35:47 -0300

    Saved in the localStorage and readme created

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue-crud-localstorage
+_first application created with vue using the localstorage to save the information of some notes_
+
+## Screenshot 📌
+![alt text](https://github.com/franciscopugh/vue-crud-localstorage/blob/master/docs/vue.png)
--- main.js
+++ main.js
@@ -13,13 +13,25 @@ const app = new Vue({
                 name: this.newTask,
                 state: false
             });
-            this.newTask = ''
+            this.newTask = '';
+            localStorage.setItem('gym-vue', JSON.stringify(this.task));
         },
         editTask: function(index){
             this.task[index].state = true;
+            localStorage.setItem('gym-vue', JSON.stringify(this.task));
         },
         deleteTask: function(index){
             this.task.splice(index, 1);
+            localStorage.setItem('gym-vue', JSON.stringify(this.task));
+        }
+    },
+    
+    created: function(){
+        let dataDB = JSON.parse(localStorage.getItem('gym-vue'))
+        if(dataDB === null){
+            this.task = [];
+        } else {
+            this.task = dataDB;
         }
     }
 
